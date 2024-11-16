7661caf256c546cda1f9599094a3649b49b1092b
Tomaz Fu Shao Yang <tomazfushaoyang@gmail.com>
2024-11-16 18:22:24 +0800

    first attempt

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,21 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5489cc109dde265cf0a7a4a1c924fe3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:roottoor@localhost/coursepdfextractor'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://TomazHayden:roottoor@TomazHayden.mysql.pythonanywhere-services.com/TomazHayden$CourseXcel'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_recycle': 200,
+    'pool_pre_ping': True,
+    'pool_size': 10,
+    'max_overflow': 5
+}
 
 db = SQLAlchemy(app)
-app.app_context().push()
 bcrypt = Bcrypt(app)
-    
-from app import routes
+
+# Clean up connections
+db.session.remove()
+db.engine.dispose()
+
+from app import routes, subject_routes
